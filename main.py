1e6767efa56cf494a5f332dc487939fb3206ca51
Rob Downing <redowning@gmail.com>
2026-04-11 23:24:58 -0700

    up and down arrows move left paddle

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     clock = pygame.time.Clock()
 
     running = True
+    paddle_y = 250
+    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,7 +18,13 @@ def main():
 
         clock.tick(60)
         screen.fill((0, 0, 0))
-        pygame.draw.rect(screen, (255, 255, 255), (50, 250, 10, 100))
+ 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            paddle_y -= 5
+        if keys[pygame.K_DOWN]:
+            paddle_y += 5
+        pygame.draw.rect(screen, (255, 255, 255), (50, paddle_y, 10, 100))
         pygame.display.flip()
     
     pygame.quit()
